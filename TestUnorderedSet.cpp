f7cb639a6bd6dbb60b6f42f25d2a25fb949b8f51
liutao <lightjames@163.com>
2018-03-01 17:23:51 +0800

    add unordered_set test

---
+++ TestUnorderedSet.cpp
@@ -0,0 +1,25 @@
+#include <gtest/gtest.h>
+#include <unordered_set>
+#include <iostream>
+
+class UnorderedSetTest : public ::testing::Test {
+    protected:
+        virtual void SetUp() {
+            _mUnorderedSet.clear(); 
+        }
+
+        virtual void TearDown() {
+        }
+    std::unordered_set<int> _mUnorderedSet;
+};
+
+TEST_F(UnorderedSetTest, traverse) {
+    for (int i = 0; i < 5; ++i) {
+        _mUnorderedSet.insert(i);
+    }
+
+    for (int el : _mUnorderedSet)
+        std::cout << el << " ";
+    std::cout << std::endl;
+    ASSERT_EQ(5, _mUnorderedSet.size());
+}
